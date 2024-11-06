ecd2784fadaa22e2580a79e44b4b834e674f5556
chason <chason001@126.com>
2024-11-06 18:00:08 +0800

    临时提交

--- src/main/java/com/chason/algrithm/class05/_05_CrossLinked.java
+++ src/main/java/com/chason/algrithm/class05/_05_CrossLinked.java
@@ -11,13 +11,95 @@ import java.util.Set;
  */
 public class _05_CrossLinked {
 
+
+    public static void main(String[] args) {
+
+        Node head = new Node(1);
+        head.next = new Node(2);
+        Node in = head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        Node last = head.next.next.next.next.next = new Node(6);
+        last.next = in;
+
+        Node node = getFirstCircle2(head);
+        System.out.println(node.val);
+    }
+
     /*
         Step1: 针对每个单链表，如果有环，返回第一个入环的位置；如果没环，返回null
-
-
+        Step2: 讨论可能出现的情况：
+        1. 两个链表都是无环的 使用容器的方法
      */
     public static Node getFirstCross(Node head1, Node head2) {
 
+        Node loop1 = getFirstCircle2(head1);
+        Node loop2 = getFirstCircle2(head2);
+
+        Set<Node> set = new HashSet<>();
+        if (loop1 == null && loop2 == null) {
+
+            Node curr = head1;
+
+
+        }
+
+        return null;
+    }
+
+    // 确定两个链表无环，找第一个相交节点
+    /*
+        1. 首先遍历一次链表1 记录长度 a 和 链表的最末尾节点
+        2. 遍历一次链表2 记录长度 b 和链表的最末尾节点
+        3. 如果他们的末尾节点是同一个，则两链表相交，否则不相交
+        4. 如果两链表相交 求长度差 c = a -b; 让长的那个先走c步，然后再两个同时出发，两个链表的同一个节点就是相交的点
+     */
+    public static Node noLoop(Node head1, Node head2) {
+
+        if (head1 == null || head2 == null) {
+            return null;
+        }
+
+        int length1 = 0;
+        Node end1;
+        Node curr = head1;
+        while (curr != null) {
+            length1++;
+            curr = curr.next;
+        }
+        end1 = curr;
+
+        int length2 = 0;
+        Node end2;
+        curr = head2;
+        while (curr != null) {
+            length2++;
+            curr = curr.next;
+        }
+        end2 = curr;
+
+        if (end1 != end2) { // 不相交
+            return null;
+        }
+
+        int step = Math.abs(length1 - length2);
+        Node start = length1 >= length2 ? head1 : head2;
+        Node other = length1 < length2 ? head1 : head2;
+
+        curr = start;
+        while (step > 0) {
+            curr = curr.next;
+            step--;
+        }
+
+        while (curr != null) {
+            if (curr == other) {
+                return curr;
+            }
+
+            curr = curr.next;
+            other = other.next;
+        }
 
         return null;
     }
@@ -50,7 +132,7 @@ public class _05_CrossLinked {
      */
     public static Node getFirstCircle2 (Node head) {
 
-        if (head == null || head.next == null) {
+        if (head == null || head.next == null || head.next.next == null) {
             return null;
         }
 
@@ -58,11 +140,9 @@ public class _05_CrossLinked {
         Node slow = head;
 
         while (fast != null) {
-
             if (fast.next == null) {
                 return null;
             }
-
             fast = fast.next.next;
             slow = slow.next;
 
