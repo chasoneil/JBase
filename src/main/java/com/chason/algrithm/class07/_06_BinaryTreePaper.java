b07f1fcc46c9a64d7e68c062e610d57c925c085c
chason <chason001@126.com>
2025-01-24 16:54:15 +0800

    二叉树的一些题，深入理解递归；二叉树的树形DP套路练习1

--- src/main/java/com/chason/algrithm/class07/_06_BinaryTreePaper.java
+++ src/main/java/com/chason/algrithm/class07/_06_BinaryTreePaper.java
@@ -7,10 +7,39 @@ package com.chason.algrithm.class07;
 public class _06_BinaryTreePaper {
 
     public static void main(String[] args) {
-
-
+        int N = 3;
+        printLine(N);
     }
 
+    /*
+     这个其实是一个有规律的
+     1. 头节点是凹
+     2. 所有左子树的都节点都是凹
+     3. 所有右子树的头节点都是凸
+
+     参数是对折的次数 N
+     */
+    public static void printLine(int N) {
+        process(1, N, true);
+    }
+
+    /*
+     i: 当前来到的层数
+     N: 最大层数，是调用者给的，且不变
+     down: 表示当前这个节点是不是down 如果是down 则为Ture
+     想象一个节点：这个节点位于i层，目的是中序遍历这个树
+     */
+    public static void process(int i, int N, boolean down) {
+
+        if (i > N) {
+            return;
+        }
+
+        // 中序遍历的方式打印整棵树
+        process(i+1, N, true);
+        System.out.print(down ? "凹" : "凸");
+        process(i+1, N, false);
+    }
 
 
 }
---
+++ src/main/java/com/chason/algrithm/class07/_07_IsFullBinaryTree.java
@@ -0,0 +1,90 @@
+package com.chason.algrithm.class07;
+
+import com.chason.algrithm.class06.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 如何判断一棵树是不是完全二叉树
+ * 完全二叉树：
+ * 每层都是满的，或者处于从左往右依次变满的状态
+ */
+public class _07_IsFullBinaryTree {
+
+    public static void main(String[] args) {
+
+        TreeNode head = buildTestTree();
+        System.out.println(isFullBT(head));
+
+    }
+
+    /**
+     * 思路： 按照二叉树的按层遍历
+     * 1. 如果一个节点他只有右孩子，没有左孩子，他肯定不是完全二叉树
+     * 2. 如果当你遍历到一个节点的时候，他的子孩子不完整（没有子，或者只有一个左）那么他后面的节点都是叶子节点
+     * 叶子（不能有子节点了）
+     * 3. 如果满足上述要求遍历完成，则是完全二叉树
+     *
+     * @return
+     */
+    public static boolean isFullBT(TreeNode head) {
+
+        if (head == null) {
+            return true;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(head);
+
+        TreeNode tmp = null;
+        boolean leaf = false;
+        while (!queue.isEmpty()) {
+
+            tmp = queue.poll();
+
+            if (leaf) {
+                if (tmp.left != null || tmp.right != null) {
+                    return false;
+                }
+            }
+
+            if (tmp.right != null && tmp.left == null) { // 命中 1
+                return false;
+            }
+
+            if (!(tmp.left != null && tmp.right != null)) {
+                leaf = true;    // 命中 2 后面的所有节点都只能是叶子节点
+            }
+
+            if (tmp.left != null) {
+                queue.offer(tmp.left);
+            }
+
+            if(tmp.right != null) {
+                queue.offer(tmp.right);
+            }
+        }
+
+        return true;
+    }
+
+
+    private static TreeNode buildTestTree() {
+
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+
+        node1.left = node2;
+        node1.right = node3;
+        node3.left = node4;
+        node2.right = node5;
+
+        return node1;
+    }
+
+
+}
