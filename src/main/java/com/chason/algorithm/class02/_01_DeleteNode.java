036c36bd43d1bc02723a880b0a827844387bbad7
chason <chason001@126.com>
2025-02-18 20:51:04 +0800

    review linkedlist

--- src/main/java/com/chason/algorithm/class02/_01_DeleteNode.java
+++ src/main/java/com/chason/algorithm/class02/_01_DeleteNode.java
@@ -1,31 +1,32 @@
 package com.chason.algorithm.class02;
 
-import com.chason.algorithm.class02.base.Node;
-import com.chason.algorithm.utils.LinkedUtils;
-
 /**
  * delete target node from single linked list
  */
 public class _01_DeleteNode {
 
-    public static Node<Integer> deleteNode(Node<Integer> head, int target) {
+    // 删除节点涉及删除头
+    public static Node delNode(Node head, int target) {
 
         if (head == null) {
-            return null;
+            return head;
         }
 
-        Node<Integer> newHead = head;
+        // 来到第一个head 不是target的位置
+        Node newHead = head;
+        while (newHead != null) {
 
-        while (newHead.value == target) {
+            if (newHead.value != target) {
+                break;
+            }
             newHead = newHead.next;
         }
 
-        Node<Integer> prev = newHead;
-        Node<Integer> curr = newHead;
-
+        Node curr = newHead;
+        Node prev = newHead;
         while (curr != null) {
             if (curr.value == target) {
-                prev.next = curr.next;
+                prev.next = curr.next;   // 保证prev.next在移动
             } else {
                 prev = curr;
             }
@@ -35,16 +36,101 @@ public class _01_DeleteNode {
         return newHead;
     }
 
+
+    public static class Node {
+        int value;
+        Node next;
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    /*
+        给定一个单链表的头节点和一个target
+        返回删除这个target之后的链表长度
+     */
+    private static int getOtherNodeLength (Node head, int target) {
+
+        if (head == null) {
+            return 0;
+        }
+
+        int length = 0;
+        Node curr = head;
+        while (curr != null) {
+            length++;
+            curr = curr.next;
+        }
+
+        int targetCount = 0;
+        curr = head;
+        while (curr != null) {
+            if (curr.value == target) {
+                targetCount++;
+            }
+            curr = curr.next;
+        }
+
+        return length - targetCount;
+    }
+
+    private static int size(Node head) {
+        if (head == null) {
+            return 0;
+        }
+
+        int size = 0;
+        Node curr = head;
+        while (curr != null) {
+            size++;
+            curr = curr.next;
+        }
+
+        return size;
+    }
+
+    private static Node generateLinkedList(int maxLength, int maxValue) {
+        int rLength = (int) (Math.random() * maxLength);
+        Node head = null;
+        Node prev = head;
+        for (int i=0; i<rLength; i++) {
+            int rValue = (int)(Math.random() * maxValue) - (int)(Math.random() * maxValue);
+            Node node = new Node(rValue);
+            if (head == null) {
+                head = node;
+                prev = node;
+            } else {
+                prev.next = node;
+                prev = node;
+            }
+        }
+        return head;
+    }
+
+
+    // ========= 对数器 ============
     public static void main(String[] args) {
 
-        Node<Integer> head = LinkedUtils.buildRandomSingle(10, 10);
-        int target = -5;
+        int maxLength = 10;
+        int maxValue = 100;
+        int testTime = 100000;
+        boolean suc = true;
 
-        LinkedUtils.print(head, null);
-
-        head = deleteNode(head, target);
-        LinkedUtils.print(head, null);
-
+        for (int i=0; i<testTime; i++) {
+            Node head = generateLinkedList(maxLength, maxValue);
+            int target = (int) (Math.random() * maxValue);
+            int removeLength = getOtherNodeLength(head, target);
+            Node newHead = delNode(head, target);
+            if (newHead != null  && newHead.value == target) {
+                suc = false;
+                break;
+            }
+            if (size(newHead) != removeLength) {
+                suc = false;
+                break;
+            }
+        }
+        System.out.println(suc ? "Passed" : "Failed");
     }
 
 }
--- src/main/java/com/chason/algorithm/class02/_02_ReverseLinked.java
+++ src/main/java/com/chason/algorithm/class02/_02_ReverseLinked.java
@@ -1,8 +1,9 @@
 package com.chason.algorithm.class02;
 
 import com.chason.algorithm.class02.base.DNode;
-import com.chason.algorithm.class02.base.Node;
-import com.chason.algorithm.utils.LinkedUtils;
+
+import java.util.ArrayList;
+import java.util.Stack;
 
 /**
  *  reverse linked list
@@ -11,7 +12,7 @@ public class _02_ReverseLinked {
 
     /**
      * reverse single linked list
-     * @return
+     * @return newHead
      */
     public static Node reverseSingle(Node head) {
 
@@ -54,16 +55,85 @@ public class _02_ReverseLinked {
     }
 
 
+    public static class Node {
+        int value;
+        Node next;
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    // ===== 对数器 ====
     public static void main(String[] args) {
 
-//        Node head = LinkedUtils.buildRandomSingle(10, 10);
-//        LinkedUtils.print(head, null);
-//        Node newHead = reverseSingle(head);
-//        LinkedUtils.print(newHead, null);
-        DNode head = LinkedUtils.buildRandomDouble(10, 10);
-        LinkedUtils.print(null, head);
-        DNode newHead = reverseDouble(head);
-        LinkedUtils.print(null, newHead);
+        int maxLength = 10;
+        int maxValue = 100;
+        int testTime = 100000;
+        boolean suc = true;
+
+        for (int i=0; i<testTime; i++) {
+            Node head = generateLinkedList(maxLength, maxValue);
+            Stack<Node> stack = new Stack<>();
+            Node curr = head;
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.next;
+            }
+
+            Node newHead = reverseSingle(head);
+            curr = newHead;
+            while (curr != null) {
+                if (curr.value != stack.pop().value) {
+                    suc = false;
+                    break;
+                }
+            }
+        }
+        System.out.println(suc ? "Passed" : "Failed");
+    }
+
+
+
+    //  传入两个链表的头，判断两个链表是否是反转链表
+    private static boolean sucReverse(Node head1, Node head2) {
+
+        Stack<Integer> stack = new Stack<>();
+        while (head1 != null) {
+            stack.push(head1.value);
+            head1 = head1.next;
+        }
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        while (head2 != null) {
+            list1.add(head2.value);
+            head2 = head2.next;
+        }
+
+        for (int i=0; i<list1.size(); i++) {
+            if (list1.get(i) != stack.pop().intValue()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static Node generateLinkedList(int maxLength, int maxValue) {
+        int rLength = (int) (Math.random() * maxLength);
+        Node head = null;
+        Node prev = head;
+        for (int i=0; i<rLength; i++) {
+            int rValue = (int)(Math.random() * maxValue) - (int)(Math.random() * maxValue);
+            Node node = new Node(rValue);
+            if (head == null) {
+                head = node;
+                prev = node;
+            } else {
+                prev.next = node;
+                prev = node;
+            }
+        }
+        return head;
     }
 
 }
